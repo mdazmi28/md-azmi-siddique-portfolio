fbc9fc066e8d3a9c23264228101b9899e06aa9d6
md-azmi-siddique <md.azmi.siddique@gmail.com>
2025-01-10 15:46:55 +0600

    certification added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 "use client";
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { workData } from '../data/workData'; // Ensure correct import path
+import { certificationData } from '@/data/certificationData';
 import { educationData } from '../data/educationData'; // Ensure correct import path
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -16,7 +17,7 @@ const About = () => {
         <div className='scroll-smooth'>
             <div className='flex flex-col gap-5'>
                 <div>
-                    <h1 className='text-4xl '>Work Experience</h1>
+                    <h1 className='text-4xl pb-4'>Work Experience</h1>
                     {
                         workData?.work_experience?.map((data, index) => {
                             return (
@@ -32,9 +33,9 @@ const About = () => {
                         })
                     }
                 </div>
-                <div>
 
-                <h1 className='text-4xl'>Education</h1>
+                <div>
+                    <h1 className='text-4xl pb-4'>Education</h1>
                     {
                         educationData?.work_experience?.map((data, index) => {
                             return (
@@ -49,6 +50,39 @@ const About = () => {
                             );
                         })
                     }
+                </div>
+
+                <div className="flex flex-col gap-6">
+                    <h1 className="text-4xl pb-4">
+                        Certifications
+                    </h1>
+                    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                        {certificationData?.certificationData?.map((data, index) => (
+                            <div
+                                key={index}
+                                data-aos="zoom-in"
+                                data-aos-duration="1000"
+                                className="bg-white p-6 rounded-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 mb-6 dark:text-black">
+
+                                <a
+                                    href={data.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="absolute top-2 right-2 text-gray-400 hover:text-gray-600 transition-colors"
+                                    title="View Certificate"
+                                >
+                                    <img src="/assets/images/certifications/link.png" alt="" className='h-5 w-5'/>
+                                </a>
+                                {/* Render the image */}
+                                <img src={data.img} alt={data.certificate_name} className="w-16 h-16 mb-4" />
+
+                                {/* Render the certificate provider */}
+                                <h2 className="text-xl font-semibold">{data.certificate_name}</h2>
+                                <h2 className="text-gray-600">Provided By: {data.certificate_provider}</h2>
+                            </div>
+                        ))}
+
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/data/certificationData.js
@@ -0,0 +1,33 @@
+// workerData.js
+// import {c1} from '/assets/images/certifications/coursera-color.svg'
+const certificationData = {
+    "certificationData": [
+        {
+            "certificate_provider": "Coursera",
+            "link": "Bsc in Computer Science and Engineering",
+            "certificate_name": "HTML and CSS in depth",
+            "img":"/assets/images/certifications/meta-color.svg",
+        },
+        {
+            "certificate_provider": "Coursera",
+            "link": "Bsc in Computer Science and Engineering",
+            "certificate_name": "Programming with JavaScript",
+            "img":"/assets/images/certifications/meta-color.svg",
+        },
+        {
+            "certificate_provider": "Coursera",
+            "link": "Bsc in Computer Science and Engineering",
+            "certificate_name": "Foundations of User Experience (UX) Design",
+            "img":"/assets/images/certifications/google-bg.png",
+        },
+        {
+            "certificate_provider": "Coursera",
+            "link": "Bsc in Computer Science and Engineering",
+            "certificate_name": "Foundations of User Experience (UX) Design",
+            "img":"/assets/images/certifications/meta-color.svg",
+        },
+         
+    ]
+};
+
+export { certificationData }; // Ensure you are exporting `workData`
