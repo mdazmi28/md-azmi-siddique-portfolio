d2afa72e5ba0468d0a8cb08016410662f338ca47
md-azmi-siddique <md.azmi.siddique@gmail.com>
2025-01-10 17:55:37 +0600

    update hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,20 +8,18 @@ const HeroSection = () => {
             <div className='scroll-smooth h-screen flex flex-col gap-5 md:flex-row'>
                 {/* Left Section with Text */}
                 <div className='w-full md:w-1/2 text-center md:text-left flex flex-col justify-center order-2 md:order-1'>
-                    <h1 className='text-5xl font-bold  mb-4'>
+                    <h1 className='text-xl md:text-5xl font-bold  mb-4'>
                         Md Azmi Siddique
                     </h1>
                     <p className='text-lg mb-6'>
                         Software Engineer | Frontend Developer | Problem Solver
                     </p>
                     <TypeAnimation
-                        className='text-2xl font-semibold  mb-6'
+                        className='text-xl md:text-2xl font-semibold  mb-6'
                         sequence={[
                             'Building intuitive web experiences.',
                             1000,
                             'Crafting innovative solutions for the web.',
-                            1000,
-                            'Creating scalable and efficient applications.',
                             1000,
                             'Delivering impactful software solutions.',
                             1000
@@ -29,12 +27,30 @@ const HeroSection = () => {
                         wrapper="span"
                         speed={50}
                         style={{
-                            fontSize: '2em',
+                            fontSize: '',
                             display: 'inline-block',
                             // color: 'white'
                         }}
                         repeat={Infinity}
                     />
+                    <div className="flex flex-row gap-4 items-center justify-center md:items-start md:justify-start">
+
+                        <div className='h-10 w-10'>
+                            <a href="https://github.com/mdazmi28/" target='_blank' rel='noopener noreferrer' className='cursor-pointer'>
+                                <img src="https://img.icons8.com/?size=100&id=63777&format=png&color=000000" alt="Github" />
+                            </a>
+                        </div>
+                        <div className='h-10 w-10'>
+                            <a href="https://www.linkedin.com/in/mdazmi28/" target='_blank' rel='noopener noreferrer' className='cursor-pointer'>
+                                <img src="https://img.icons8.com/?size=100&id=xuvGCOXi8Wyg&format=png&color=000000" alt="LinkedIn" />
+                            </a>
+                        </div>
+
+
+
+
+
+                    </div>
                 </div>
 
                 {/* Right Section with Image */}
@@ -43,7 +59,7 @@ const HeroSection = () => {
                         <img
                             src='/assets/images/my_img.png'
                             alt='Your Company Logo'
-                            className='shadow-xl object-cover h-[400px] w-auto'
+                            className='shadow-xl object-cover mt-52 md:mt-0 h-32 md:h-[400px] w-auto'
                         />
                     </div>
                 </div>
