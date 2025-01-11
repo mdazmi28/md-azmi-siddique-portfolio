a32de31a2b5c8f3adafbce4eef6d42343b7720e3
md-azmi-siddique <md.azmi.siddique@gmail.com>
2025-01-11 11:24:47 +0600

    Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { projectData } from '../data/projectData';
 
 const Projects = () => {
-    return (
-        <div>
-            hello
+  return (
+    <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {projectData.map((project, index) => (
+        <div
+          key={index}
+          className=" bg-white dark:bg-gray-800 
+                 text-gray-800 dark:text-gray-200 rounded-lg shadow-md p-4 flex flex-col items-center"
+        >
+          <img
+            src={project.image}
+            alt={project.name}
+            className="w-full h-48 object-cover rounded-md mb-4"
+          />
+          <h3 className="text-xl font-semibold">{project.name}</h3>
+          <p className="text-gray-600 text-sm my-2">{project.description}</p>
+          <div className="flex gap-4 mt-4">
+            <a
+              href={project.liveLink}
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live
+            </a>
+            <a
+              href={project.githubLink}
+              className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-900"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
---
+++ src/data/projectData.js
@@ -0,0 +1,26 @@
+const projectData = [
+    {
+      "name": "Tele Cash Website Prototype",
+      "image": "/assets/images/projects/portfolio.png",
+      "description": "A personal portfolio website for TeleCash",
+      "liveLink": "https://tele-cash.vercel.app/",
+      "githubLink": ""
+    },
+    {
+      "name": "E-Commerce Landing Page",
+      "image": "/assets/images/projects/ecommerce.png",
+      "description": "A full-stack e-commerce web app with user authentication, product listings, and a shopping cart.",
+      "liveLink": "https://e-commerce-landing-page-ui.vercel.app/",
+      "githubLink": "https://github.com/mdazmi28/E-Commerce-Landing-Page-UI"
+    },
+    {
+      "name": "Chat Application",
+      "image": "/assets/images/projects/chat-app.png",
+      "description": "A real-time chat application built using React, Node.js, and Socket.io.",
+      "liveLink": "https://chatapp.com",
+      "githubLink": "https://github.com/username/chat-application"
+    },
+  ];
+  
+  export { projectData };
+  
