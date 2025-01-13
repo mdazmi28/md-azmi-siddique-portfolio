bd45a868d22102a5a349284374b9829240d6f0b7
mdazmi28 <md.azmi.siddique@gmail.com>
2025-01-13 10:02:00 +0600

    project page upate

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,24 +1,24 @@
 const projectData = [
     {
       "name": "Tele Cash Website Prototype",
-      "image": "/assets/images/projects/portfolio.png",
+      "image": "/assets/images/telecash.png",
       "description": "A personal portfolio website for TeleCash",
       "liveLink": "https://tele-cash.vercel.app/",
       "githubLink": ""
     },
     {
       "name": "E-Commerce Landing Page",
-      "image": "/assets/images/projects/ecommerce.png",
+      "image": "/assets/images/e_com_web.png",
       "description": "A full-stack e-commerce web app with user authentication, product listings, and a shopping cart.",
       "liveLink": "https://e-commerce-landing-page-ui.vercel.app/",
       "githubLink": "https://github.com/mdazmi28/E-Commerce-Landing-Page-UI"
     },
     {
-      "name": "Chat Application",
-      "image": "/assets/images/projects/chat-app.png",
-      "description": "A real-time chat application built using React, Node.js, and Socket.io.",
-      "liveLink": "https://chatapp.com",
-      "githubLink": "https://github.com/username/chat-application"
+      "name": "News Portal",
+      "image": "/assets/images/news_page.png",
+      "description": "A real-time news applicaiton webiste",
+      "liveLink": "",
+      "githubLink": "https://github.com/mdazmi28/Full_Stack_Blog_Web_App"
     },
   ];
   
